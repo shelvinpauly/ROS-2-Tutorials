30b6c69581db6e3bc74ffe3addeaef4d34c2e9ab
Shelvin Pauly <spauly@umd.edu>
2022-11-16 10:58:53 -0500

    Writing a simpleservice client in c++ and py, and creating custom msg and srv files

---
+++ build/tutorial_interfaces/CMakeFiles/tutorial_interfaces__rosidl_generator_py.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/tutorial_interfaces__rosidl_generator_py.dir/rosidl_generator_py/tutorial_interfaces/msg/_num_s.c.o"
+  "CMakeFiles/tutorial_interfaces__rosidl_generator_py.dir/rosidl_generator_py/tutorial_interfaces/msg/_sphere_s.c.o"
+  "CMakeFiles/tutorial_interfaces__rosidl_generator_py.dir/rosidl_generator_py/tutorial_interfaces/srv/_add_three_ints_s.c.o"
+  "rosidl_generator_py/tutorial_interfaces/libtutorial_interfaces__rosidl_generator_py.pdb"
+  "rosidl_generator_py/tutorial_interfaces/libtutorial_interfaces__rosidl_generator_py.so"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/tutorial_interfaces__rosidl_generator_py.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
